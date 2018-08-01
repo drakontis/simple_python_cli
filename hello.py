6ba67ca9fa4c6aa7ae6a9f54dcfa51ccfe86177d
Drakontis Panagiotis <drakontis.panagiotis@gmail.com>
2018-08-01 12:25:47 +0300

    Add docstring

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ import click
 @click.option('--verbose', is_flag=True, help="Will print verbose messages.")
 @click.option('--name', multiple=True, default='', help='Who are you?')
 def cli(verbose, name):
+    """This is an example script to learn Click."""
     if verbose:
         click.echo("We are in the verbose mode.")
     click.echo("Hello World")

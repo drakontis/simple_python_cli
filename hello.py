eaca921c709d384806d48c1691a6374b1ec87054
Drakontis Panagiotis <drakontis.panagiotis@gmail.com>
2018-08-01 12:09:31 +0300

    Support multiple names.

--- hello.py
+++ hello.py
@@ -2,7 +2,10 @@ import click
 
 @click.command()
 @click.option('--verbose', is_flag=True, help="Will print verbose messages.")
-def cli(verbose):
+@click.option('--name', multiple=True, default='', help='Who are you?')
+def cli(verbose, name):
     if verbose:
         click.echo("We are in the verbose mode.")
     click.echo("Hello World")
+    for n in name:
+        click.echo('Bye {0}'.format(n))

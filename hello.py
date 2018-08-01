34afa49dbf43135e53dcaa626fe1f8f917372900
Drakontis Panagiotis <drakontis.panagiotis@gmail.com>
2018-08-01 12:30:16 +0300

    Require arguments.

--- hello.py
+++ hello.py
@@ -4,11 +4,12 @@ import click
 @click.option('--verbose', is_flag=True, help="Will print verbose messages.")
 @click.option('--name', multiple=True, default='', help='Who are you?')
 @click.password_option()
-def cli(verbose, name, password):
+@click.argument('country')
+def cli(verbose, name, password, country):
     """This is an example script to learn Click."""
     if verbose:
         click.echo("We are in the verbose mode.")
-    click.echo("Hello World")
+    click.echo("Hello {0}".format(country))
     for n in name:
         click.echo('Bye {0}'.format(n))
     click.echo('We received {0} as password. (I print password for testing purposes. :) )'.format(password))
